f83192a11d57bfe2390973be5fe4de99ff866317
iczopl1 <przystas.ireneusz@gmail.com>
2025-03-04 11:34:27 +0100

    Naprawa python

--- lua/iczo/plugins/dap.lua
+++ lua/iczo/plugins/dap.lua
@@ -1,5 +1,16 @@
 return{
-{
+  {
+    "mfussenegger/nvim-dap-python",
+    ft="python",
+    dependencies ={
+      "mfussenegger/nvim-dap",
+    },
+    config = function ()
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+    end
+  },
+  {
     "rcarriga/nvim-dap-ui",
     event = "VeryLazy",
     dependencies = "mfussenegger/nvim-dap",
