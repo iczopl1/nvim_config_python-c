9e35ce99aaa1716804dd28f2d4c302737961eb2a
iczopl1 <przystas.ireneusz@gmail.com>
2025-03-06 20:20:30 +0100

    poprawa vimtex

---
+++ lua/iczo/plugins/vimtex.lua
@@ -0,0 +1,11 @@
+return{
+    { 
+    "lervag/vimtex",
+  lazy = false,     -- we don't want to lazy load VimTeX
+ 
+    config = function()
+      vim.g.vimtex_view_method = 'zathura'  -- Use Zathura as the PDF viewer
+      vim.g.vimtex_compiler_method = 'latexrun'  -- Use latexrun as the compiler
+    end
+  }
+}
