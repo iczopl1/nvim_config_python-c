99712fb885b6d4669e39cd7483d6ee6d2bd1824c
iczopl1 <przystas.ireneusz@gmail.com>
2025-05-07 12:56:55 +0200

    mała poprawka mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,8 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map('', '<PageUp>', '<Nop>', { noremap = true, silent = true })
-map('', '<PageDown>', '<Nop>', { noremap = true, silent = true })
-map('n','<leader>q',':bd<CR>',{desc = "Close current buffor"})
-map('n', '<leader>qa', ':bufdo bd<CR>', { desc = "Close all" })
+map({'','i'}, '<PageUp>', '<Nop>', { noremap = true, silent = true })
+map({'','i'}, '<PageDown>', '<Nop>', { noremap = true, silent = true })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
